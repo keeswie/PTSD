e22ddc26afb0c1b46b1dff7457ab8133a74e7c3c
kees <72143341+keeswie@users.noreply.github.com>
2021-12-26 23:00:46 +0100

    made my part of the openAPI
    added the whole employee side

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 
 connectDB();
 const Port = process.env.Port || 3000;
-
+//constructs the app (aka server)
 const server = http.createServer(app);
 
 server.listen(Port);
